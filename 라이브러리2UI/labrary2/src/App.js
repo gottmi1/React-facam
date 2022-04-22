322877715ec41ccfc2d9c6d22e95c99fc051bd0e
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-04-22 21:34:55 +0900

    react bootstrap

--- 라이브러리2UI/labrary2/src/App.js
+++ 라이브러리2UI/labrary2/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 // import AntDesign from "./components/AntDesign/AntDesign";
 import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapExample from "./components/ReactBootstrap/BootstrapExample";
 
 function App() {
-  return <div className="App">{/* <AntDesign /> */}</div>;
+  return (
+    <div className="App">
+      <BootstrapExample />
+    </div>
+  );
 }
 
 export default App;
---
+++ 라이브러리2UI/labrary2/src/components/ReactBootstrap/BootstrapExample.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AlertDismissible from './AlertDismissible'
+import ButtonExample from './ButtonExample'
+import GridExample from './GridExample'
+
+export default function BootstrapExample() {
+  return (
+    <>
+    <AlertDismissible />
+    <GridExample />
+    <ButtonExample />
+    </>
+  )
+}
