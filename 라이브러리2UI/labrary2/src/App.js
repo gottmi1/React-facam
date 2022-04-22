657e427372614afb5d62b93eba8a561950136702
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-04-23 01:05:12 +0900

    Material UI ing...

--- 라이브러리2UI/labrary2/src/App.js
+++ 라이브러리2UI/labrary2/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 // import AntDesign from "./components/AntDesign/AntDesign";
-import "bootstrap/dist/css/bootstrap.min.css";
-import BootstrapExample from "./components/ReactBootstrap/BootstrapExample";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import BootstrapExample from "./components/ReactBootstrap/BootstrapExample";
+import MaterialExample from "./components/Material/MaterialExample";
 
 function App() {
   return (
     <div className="App">
-      <BootstrapExample />
+      {/* <BootstrapExample /> */}
+      <MaterialExample />
     </div>
   );
 }
---
+++ 라이브러리2UI/labrary2/src/components/Material/MaterialExample.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MaterialButton from "./MaterialButton"
+import CenteredGrid from './CenteredGrid'
+
+export default function MaterialExample() {
+return <>
+  <MaterialButton />
+  <CenteredGrid />
+</>
+}
