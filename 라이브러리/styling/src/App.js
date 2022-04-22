f1c21353119346f7a521cba6c12aca1fc6c44125
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-04-22 17:25:05 +0900

    스타일링 라이브러리

--- 라이브러리/styling/src/App.js
+++ 라이브러리/styling/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import EmotionExample from "./components/EmotionExample/EmotionExample";
+// import EmotionExample from "./components/EmotionExample/EmotionExample";
 // import StyledComponentExample from "./components/StyledComponents/StyledComponentExample";
+import SassExample from "./components/Sass/SassExample";
 
 function App() {
   return (
     <div className="App">
       {/* <StyledComponentExample /> */}
-      <EmotionExample />
+      {/* <EmotionExample /> */}
+      <SassExample />
     </div>
   );
 }
---
+++ 라이브러리/styling/src/components/Sass/SassExample.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './SassExample.scss'
+
+export default function SassExample() {
+  return (
+    <div>
+      <p>Example</p>
+      <p className="font">Example</p>
+      <p className="text">Example</p>
+      <nav>
+        <ul>
+          <li>으악</li>
+          <li>
+            <a href="#">윽</a></li>
+        </ul>
+      </nav>
+      <p className="base">으윽</p>
+      <p className="inverse">으윽</p>
+      <p className="info">info</p>
+      <p className="alert">alert</p>
+      <p className="success">success</p>
+      <p className="huhu">huhu</p>
+      <p className="message">message</p>
+      <p className="error">error</p>
+      <p className="success2">success2</p>
+      <p className="warning">warning</p>
+      <div className="square-av">ㅋ</div>
+      <div className="circle-av">ㅋ</div>
+      
+    </div>
+  )
+}
