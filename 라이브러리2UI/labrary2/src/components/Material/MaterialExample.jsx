46bb122e33909c45ee13214da32b1d154f3e4a57
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-04-23 20:58:21 +0900

    Matrial UI

--- 라이브러리2UI/labrary2/src/components/Material/MaterialExample.jsx
+++ 라이브러리2UI/labrary2/src/components/Material/MaterialExample.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import MaterialButton from "./MaterialButton"
 import CenteredGrid from './CenteredGrid'
+import IconLabelButtons from './IconLabelButtons'
+import CheckboxLabels from './CheckBoxLabel'
 
 export default function MaterialExample() {
 return <>
   <MaterialButton />
   <CenteredGrid />
+  <IconLabelButtons />
+  <CheckboxLabels />
 </>
 }
